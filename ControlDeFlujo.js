ce0ec074b0c63a4266d8ce7f0241db040783634f
Alex <marcosscotaro1315@gmial.com>
2023-02-22 19:20:22 -0300

    segundo dia, ControldeFlujo

---
+++ ControlDeFlujo.js
@@ -0,0 +1,26 @@
+function viajar (destino){
+    if (destino === "Brasil" ) {
+        console.log("Gire a la derecha");
+    } else if ( destino === "Argentina") {
+        console.log ("Gire a la izquierda"); 
+    } else {
+        console.log("Nos Perdimos")
+    }
+}
+
+//viajar ("Brasil")
+
+//viajar ("ArgentinaCampeon")
+
+//viajar ("chile")
+
+
+function puedeManejar (edad) {
+    if (edad >= 18 ) {
+        console.log("Si puede");
+    } else if (edad < 18 ) {
+        console.log("No puede")
+    }
+}
+
+puedeManejar (17)
